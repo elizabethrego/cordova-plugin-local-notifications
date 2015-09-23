a6699e541c4c4ae4221204064a09cf94dbe0a2c1
William Neely <william@piecewise.com>
2015-09-23 08:58:13 -0400

    undeclared identifier error

--- src/ios/APPLocalNotification.m
+++ src/ios/APPLocalNotification.m
@@ -648,7 +648,7 @@
 
     [center addObserver:self
                selector:@selector(didRegisterUserNotificationSettings:)
-                   name:UIApplicationRegisterUserNotificationSettings
+                   name:UIApplicationDidRegisterUserNotificationSettings
                  object:nil];
 }
 
